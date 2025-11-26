1d1802482e6d22a637badb902afec8cd3880790f
= <=>
2025-11-26 19:51:42 +0100

    add method getServiceList in serviceFetcher, create class service

---
+++ service.py
@@ -0,0 +1,5 @@
+class service:
+    def __init__(self, serviceId, description, state):
+        self.serviceId = serviceId
+        self.description = description
+        self.state = state
--- servicefetcher.py
+++ servicefetcher.py
@@ -1,14 +1,24 @@
 import os
+import service
 
 def fetchServices(status):
-    command = "systemctl --type=service #argument | awk '{print $1 "  ";"  "$2"  ";"  "$3"  ";"  "$4" ";" "$5}'"
+    command = "systemctl --type=service #argument | awk '{print $1 " "," "$2" "," "$3" "," "$4" "," "$5" ";}'"
     command = command.replace("#argument", printStatus(status))
-    serviceList = os.system(command)
+    serviceList = os.popen(command).read()
     return serviceList
-    
+
 
 def printStatus(status):
     if status == "running":
         return "--state=running"
     if status == "all":
         return ""
+
+def getServiceList(status):
+    serviceList = []
+    rawList = fetchServices(status)
+    for line in rawList.splitlines():
+        lineSplit = line.split(" ")
+        if not line.startswith("UNIT"):
+            serviceList.append(service.service(str(lineSplit[0]), str(lineSplit[4]), str(lineSplit[2])))
+    return serviceList
