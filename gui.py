d5ec743a88bc44571ed13b5d552eb9371913ef76
= <=>
2025-11-26 20:10:54 +0100

    add app config

--- gui.py
+++ gui.py
@@ -1,12 +1,16 @@
 import gi
+import json
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import GLib, Gtk
 
+with open("config/app.json", mode="r", encoding="utf-8") as file:
+     appInfos = json.load(file)
+
 class Application(Gtk.Application):
     def __init__(self):
-        super().__init__(application_id="com.magicwilbur.Butler")
-        GLib.set_application_name("Butler")
+        super().__init__(application_id=appInfos["applicationId"])
+        GLib.set_application_name(appInfos["applicationName"])
 
     def do_activate(self):
         window = Gtk.ApplicationWindow(application=self, title="Butler")
